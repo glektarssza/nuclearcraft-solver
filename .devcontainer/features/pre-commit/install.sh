79d8efdfbeaa09bee2b03d725512b9e7df3a6978
G'lek Tarssza <glek@glektarssza.com>
2025-01-23 19:19:56 +0000

    Use function lib in pre-commit install script.

--- .devcontainer/features/pre-commit/install.sh
+++ .devcontainer/features/pre-commit/install.sh
@@ -4,18 +4,10 @@
 . ./functions.sh;
 
 # Set up Python 3 venv.
-su ${_REMOTE_USER} -c "python3 -m venv ~${_REMOTE_USER}/.python3-venv" && \
-# Disable Python 3 venv prompt in .zshrc.
-su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.zshrc ]; then echo 'VIRTUAL_ENV_DISABLE_PROMPT=\"true\"' >> ~${_REMOTE_USER}/.zshrc; fi"  && \
-# Add Python 3 venv to .zshrc.
-su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.zshrc ]; then echo \"source \"\\\$HOME/.python3-venv/bin/activate\"\" >> ~${_REMOTE_USER}/.zshrc; fi"  && \
-# Disable Python 3 venv prompt in .bashrc.
-su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.bashrc ]; then echo 'VIRTUAL_ENV_DISABLE_PROMPT=\"true\"' >> ~${_REMOTE_USER}/.bashrc; fi"  && \
-# Add Python 3 venv to .bashrc.
-su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.bashrc ]; then echo \"source \"\\\$HOME/.python3-venv/bin/activate\"\" >> ~${_REMOTE_USER}/.bashrc; fi"  && \
-# Disable Python 3 venv prompt in .profile no .bashrc/.zshrc exists.
-su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.profile -a ! -f ~${_REMOTE_USER}/.zshrc -a ! -f ~${_REMOTE_USER}/.bashrc ]; then echo 'VIRTUAL_ENV_DISABLE_PROMPT=\"true\"' >> ~${_REMOTE_USER}/.profile; fi"  && \
-# Add Python 3 venv to .profile if no .bashrc/.zshrc exists.
-su ${_REMOTE_USER} -c "if [ -f ~${_REMOTE_USER}/.profile -a ! -f ~${_REMOTE_USER}/.zshrc -a ! -f ~${_REMOTE_USER}/.bashrc ]; then echo \"source \"\\\$HOME/.python3-venv/bin/activate\"\" >> ~${_REMOTE_USER}/.profile; fi"  && \
+run_as_interactive_remote_user "python3 -m venv ~/.python3-venv" && \
+# Disable Python 3 venv prompt in shells.
+install_to_shell_profiles 'VIRTUAL_ENV_DISABLE_PROMPT="true"' && \
+# Add Python 3 venv to shells.
+install_to_shell_profiles 'source "$HOME/.python3-venv/bin/activate"' && \
 # Install pre-commit.
-su ${_REMOTE_USER} -c "~${_REMOTE_USER}/.python3-venv/bin/pip3 install pre-commit";
+run_as_interactive_remote_user  "pip install pre-commit";
