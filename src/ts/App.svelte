721b3882efbe1d2a9829a3a2a4aebb75f376d33a
G'lek Tarssza <glek@glektarssza.com>
2025-01-31 16:01:17 +0000

    Doing some frameworking.

--- src/ts/App.svelte
+++ src/ts/App.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+    import Navbar from './Navbar.svelte';
+</script>
+
+<Navbar />
 <h1>Hello world!</h1>
---
+++ src/ts/Navbar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts"></script>
+
+<nav class="navbar">
+    <div class="container">
+        <span class="navbar-brand">Nuclearcraft Solver</span>
+    </div>
+</nav>
+
+<style lang="scss">
+    .container {
+        width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: 0.75rem;
+        padding-right: 0.75rem;
+    }
+
+    .navbar {
+        padding: 0.5rem 0;
+        background-color: #333;
+
+        .navbar-brand {
+            margin-right: 1rem;
+            padding: 0.125rem 0;
+            color: #fff;
+            font-size: 1.25rem;
+        }
+    }
+</style>
